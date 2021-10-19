e0b58096e70a5ac176da9bc49cfc44c808903766
wangkui0508 <wangkui0508@hotmail.com>
2021-10-19 09:48:01 +0800

    Change zoom

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,15 +1,6 @@
 <template>
-  <h2 style="background: #E8E8E8; width: 360px; margin: auto; border-radius: 25px; text-align: center; font-size: 24px;">
-  <span style="color: red">B</span>
-  <span style="color: orange">L</span>
-  <span style="color: yellow">O</span>
-  <span style="color: green">C</span>
-  <span style="color: cyan">K</span>
-  <span style="color: blue">C</span>
-  <span style="color: purple">H</span>
-  <span style="color: red">A</span>
-  <span style="color: orange">I</span>
-  <span style="color: yellow">N</span>&nbsp;
+  <div class="normal">
+  <p style="background: #E8E8E8; width: 220px; margin: auto; border-radius: 25px; text-align: center; font-size: 24px;">
   <span style="color: green">C</span>
   <span style="color: cyan">H</span>
   <span style="color: blue">E</span>
@@ -19,7 +10,8 @@
   <span style="color: yellow">O</span>
   <span style="color: green">O</span>
   <span style="color: cyan">K</span>
-  </h2>
+  </p>
+  </div>
   <div id="nav" style="font-size: 24px; width: 600px; margin: auto">
     <router-link style="text-decoration: none;" tag="li" to="/receive">receive</router-link><span style="color: #EEEEEE"> | </span>
     <router-link style="text-decoration: none;" tag="li" to="/write">write</router-link><span style="color: #EEEEEE"> | </span>
